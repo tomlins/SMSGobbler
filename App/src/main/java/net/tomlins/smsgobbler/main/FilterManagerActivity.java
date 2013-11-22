a36103aa39a9c483736ce204b45a33906a4d99f9
jason <jason.tomlins@gmail.com>
2013-11-22 17:50:36 +1000

    Refactored to API level 16 to allow use of SMS_RECEIVED broadcast. Added dialog to add new number, therefore turning receiver on or off.

--- App/src/main/java/net/tomlins/smsgobbler/main/FilterManagerActivity.java
+++ App/src/main/java/net/tomlins/smsgobbler/main/FilterManagerActivity.java
@@ -2,10 +2,15 @@ package net.tomlins.smsgobbler.main;
 
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+
 import net.tomlins.smsgobbler.R;
 
 public class FilterManagerActivity extends FragmentActivity implements ActionBar.TabListener {
@@ -18,6 +23,9 @@ public class FilterManagerActivity extends FragmentActivity implements ActionBar
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_manager);
+
+        final SharedPreferences settings = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        boolean filterOn = settings.getBoolean("filterOn", false);
 
         // Set up the action bar and tab viewer
         //
@@ -51,6 +59,29 @@ public class FilterManagerActivity extends FragmentActivity implements ActionBar
             public void onPageScrollStateChanged(int arg0) {
             }
         });
+
+        if(!filterOn) {
+            // show dialog to ask user if want to add numbers
+            new AlertDialog.Builder(this)
+                    .setTitle("No Spam Filters In Effect")
+                    .setMessage("Would you like to add a new number to block?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //startActivity(intent);
+                            SharedPreferences.Editor edit = settings.edit();
+                            edit.putBoolean("filterOn", true);
+                            edit.apply();
+
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    }).show();
+
+        }
+
     }
 
     @Override
--- App/src/main/java/net/tomlins/smsgobbler/receivers/SMSBroadcastReceiver.java
+++ App/src/main/java/net/tomlins/smsgobbler/receivers/SMSBroadcastReceiver.java
@@ -3,6 +3,9 @@ package net.tomlins.smsgobbler.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
@@ -16,31 +19,73 @@ public class SMSBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context _context, Intent _intent) {
 
+        SharedPreferences settings = _context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+        Boolean filterOn = settings.getBoolean("filterOn", false);
+
         Log.i(TAG, "SMS received");
 
-        if(_intent.getAction().equals(SMS_RECEIVED)) {
+        if(_intent.getAction().equals(SMS_RECEIVED) && filterOn) {
             Bundle bundle = _intent.getExtras();
 
+            boolean deleted = false;
             if(bundle!=null) {
+
                 Object[] pdus = (Object[])bundle.get("pdus");
-                StringBuffer smsMessageText = new StringBuffer();
-                String sender = "";
+                //StringBuffer smsMessageText = new StringBuffer();
+                String address = "";
 
                 for(int index=0; index<pdus.length; index++) {
-                    SmsMessage message = SmsMessage.createFromPdu((byte[])pdus[index]);
-                    sender = message.getOriginatingAddress();
-                    smsMessageText.append(message.getMessageBody());
+                    SmsMessage SMS = SmsMessage.createFromPdu((byte[])pdus[index]);
+                    address = SMS.getOriginatingAddress();
+                    if(address.contains("55555")) {
+                        //deleteSMS(_context, address);
+                        filterSMS(address);
+                        deleted=true;
+                        continue;
+                    }
+                    //smsMessageText.append(SMS.getMessageBody());
                 }
 
-                if(smsMessageText.length()>0) {
+                if(deleted) {
                     this.abortBroadcast();
                     Toast.makeText(_context.getApplicationContext(),
-                            "SMS Received from " + sender + " says " +
-                                    smsMessageText.toString(), Toast.LENGTH_LONG).show();
+                            "Spam SMS deleted from " + address, Toast.LENGTH_LONG).show();
                 }
             }
 
         }
     }
 
+    private void filterSMS(String address) {
+        // TODO: implement filter code here
+    }
+
+    public void deleteSMS(Context context, String number) {
+        Log.i(TAG, "Attempting to delete SMS from " + number);
+
+        try {
+            Uri uriSms = Uri.parse("content://sms/inbox");
+            Cursor c = context.getContentResolver().query(uriSms, null, null, null, null);
+
+            Log.i(TAG, "Scanning SMS database...");
+            while(c.moveToNext()) {
+
+                int id = c.getInt(0);
+                int thread_id = c.getInt(1);
+                String address = c.getString(2);
+
+                Log.i(TAG, "Found SMS ->Sender:" + number + ", _id:" + id + ", thread_id:" + thread_id);
+
+                if(address.equals(number)) {
+                    Log.i(TAG, "Match found, deleting...");
+                    context.getContentResolver().delete(
+                            Uri.parse("content://sms/" + thread_id), "address=?",
+                            new String[]{String.valueOf(address)});
+                }
+            }
+        }
+        catch (Exception e) {
+            Log.e("log>>>", e.toString());
+        }
+    }
 }
